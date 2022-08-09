b895b4d220b2e3534e4b082439d6d7cd47ce1014
NDudenchenko <n.dudenchenko@gmail.com>
2022-08-10 02:12:07 +0300

    Initial commit

---
+++ Source/HASF/HASF.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HASF.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HASF, "HASF" );
+ 
---
+++ Source/HASF/HASFGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HASFGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHASFGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHASFGameMode();
+};
+
+
+
